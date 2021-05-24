f07f18c3bdf6109569fcfd43fa12e53190a8fd3c
aniqmir <aniqmir@gmail.com>
2021-05-24 20:56:43 +0500

    refresh gets address auto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Web3 from "web3";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ethers } from "ethers";
 
 import {
@@ -14,25 +14,39 @@ import { FUN_COIN_ADDRESS, FUN_COIN_ABI } from "./smartcontract/funcoin";
 function App() {
   const [address, setAddress] = useState([]);
 
-  const connectToMetaMask = async () => {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum);
-      window.ethereum.enable();
-    } else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider);
+  const connectToMetaMask = () => {
+    if (address.length === 0) {
+      if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        window.ethereum.enable();
+        updateAddress();
+      } else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+        updateAddress();
+      } else {
+        alert(
+          "Non-Ethereum browser detected. You should consider trying MetaMask!"
+        );
+      }
     } else {
-      alert(
-        "Non-Ethereum browser detected. You should consider trying MetaMask!"
-      );
+      alert("Already Connected");
     }
+  };
 
+  const updateAddress = async () => {
     const web3 = window.web3;
     const accounts = await web3.eth.getAccounts();
     if (accounts !== undefined) {
       setAddress(accounts);
-      console.log(accounts);
     }
   };
+
+  useEffect(() => {
+    if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+      updateAddress();
+    }
+  }, []);
 
   const approvefromWeb3 = async () => {
     // const accounts = await window.ethereum.enable();
